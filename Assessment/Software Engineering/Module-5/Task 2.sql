abc9ae20bb2db0e8f36285b66376a0cfe7c6a342
Riktamaster <142592594+Riktamaster@users.noreply.github.com>
2023-12-23 11:21:32 +0530

    Add files via upload

---
+++ Assessment/Software Engineering/Module-5/Task 2.sql
@@ -0,0 +1,536 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 23, 2023 at 06:47 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rikta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `city_name` varchar(50) DEFAULT NULL,
+  `lat` decimal(10,6) DEFAULT NULL,
+  `long` decimal(10,6) DEFAULT NULL,
+  `country_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `city_name`, `lat`, `long`, `country_id`) VALUES
+(1, 'Berlin', 52.520008, 13.404954, 1),
+(2, 'Belgrade', 44.787197, 20.457273, 2),
+(3, 'Zagreb', 45.815399, 15.966568, 3),
+(4, 'New York', 40.730610, -73.935242, 4),
+(5, 'Los Angeles', 34.052235, -118.243683, 4),
+(6, 'Warsaw', 52.237049, 21.017532, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(11) NOT NULL,
+  `country_name` char(50) DEFAULT NULL,
+  `country_name_eng` char(50) DEFAULT NULL,
+  `country_code` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `country_name`, `country_name_eng`, `country_code`) VALUES
+(1, 'Deutschland', 'Germany', 'DEU'),
+(2, 'Srbija', 'Serbia', 'SRB'),
+(3, 'Hrvatska', 'Croatia', 'HRV'),
+(4, 'United States of America', 'United States of America', 'USA'),
+(5, 'Polska', 'Poland', 'POL'),
+(6, 'Espana', 'Spain', 'ESP'),
+(7, 'Rossiya', 'Russia', 'RUS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `CNM` int(11) NOT NULL,
+  `CNAME` char(30) DEFAULT NULL,
+  `CITY` char(50) DEFAULT NULL,
+  `RATING` int(11) DEFAULT NULL,
+  `SNo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`CNM`, `CNAME`, `CITY`, `RATING`, `SNo`) VALUES
+(201, 'Hoffman', 'London', 100, 1001),
+(202, 'Giovanne', 'Roe', 200, 1003),
+(203, 'Liu', 'San Jose', 300, 1002),
+(204, 'Grass', 'Barcelona', 100, 1002),
+(206, 'Clemens', 'London', 300, 1007),
+(207, 'Pereira', 'Roe', 100, 1004);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(50) DEFAULT NULL,
+  `city_id` int(11) DEFAULT NULL,
+  `customer_address` varchar(50) DEFAULT NULL,
+  `next_call_date` date DEFAULT NULL,
+  `ts_inserted` datetime(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `customer_name`, `city_id`, `customer_address`, `next_call_date`, `ts_inserted`) VALUES
+(1, 'Jewelry Store', 4, 'Long Street 120', '2020-01-21', '2020-01-09 14:01:20.000'),
+(2, 'Bakery', 1, 'Kurfurstendamm 25', '2020-02-21', '2020-01-09 17:52:15.000'),
+(3, 'Cafe', 1, 'TauentzienstraBe 44', '2020-01-21', '2020-01-10 08:02:49.000'),
+(4, 'Restaurant', 3, 'Ulica lipa 15', '2020-01-21', '2020-01-10 09:20:21.000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `Employee_id` int(11) NOT NULL,
+  `First_name` varchar(30) DEFAULT NULL,
+  `Last_name` varchar(50) DEFAULT NULL,
+  `Salary` int(11) DEFAULT NULL,
+  `Joining_date` varchar(30) DEFAULT NULL,
+  `Department` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`Employee_id`, `First_name`, `Last_name`, `Salary`, `Joining_date`, `Department`) VALUES
+(1, 'John', 'Abraham', 1000000, '01-JAN-13 12:00:00 AM', 'Banking'),
+(2, 'Micheal', 'Clarke', 800000, '01-JAN-13 12:00:00 AM', 'Insurance'),
+(3, 'Roy', 'Thomas', 700000, '01-FEB-13 12:00:00 AM', 'Banking'),
+(4, 'Tom', 'Jose', 600000, '01-FEB-13 12:00:00 AM', 'Insurance'),
+(5, 'Jerry', 'Pinto', 650000, '01-FEB-13 12:00:00 AM', 'Insurance'),
+(6, 'Philip', 'Mathew', 750000, '01-JAN-13 12:00:00 AM', 'Services'),
+(7, 'Testname1', '123', 650000, '01-JAN-13 12:00:00 AM', 'Services'),
+(8, 'Testname2', 'Lname%', 600000, '01-FEB-13 12:00:00 AM', 'Insurance');
+
+--
+-- Triggers `employee`
+--
+DELIMITER $$
+CREATE TRIGGER `add_data` AFTER INSERT ON `employee` FOR EACH ROW BEGIN
+    -- Insert the new record into ViewTable
+    INSERT INTO `viewtable`(Employee_id, First_name, Last_name, Salary, Joining_date, Department)
+    VALUES (NEW.Employee_id, NEW.First_name, NEW.Last_name, NEW.Salary, NEW.Joining_date, NEW.Department);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `Rollno` int(11) DEFAULT NULL,
+  `S_code` varchar(30) DEFAULT NULL,
+  `Marks` int(11) DEFAULT NULL,
+  `P_code` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`Rollno`, `S_code`, `Marks`, `P_code`) VALUES
+(1, 'CS11', 50, 'CS'),
+(1, 'CS12', 60, 'CS'),
+(2, 'EC101', 66, 'EC'),
+(2, 'EC102', 70, 'EC'),
+(3, 'EC101', 45, 'EC'),
+(3, 'EC102', 50, 'EC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incentive`
+--
+
+CREATE TABLE `incentive` (
+  `Employee_ref_id` int(11) DEFAULT NULL,
+  `Incentive_date` varchar(30) DEFAULT NULL,
+  `Incentive_amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `incentive`
+--
+
+INSERT INTO `incentive` (`Employee_ref_id`, `Incentive_date`, `Incentive_amount`) VALUES
+(1, '01-FEB-13', 5000),
+(2, '01-FEB-13', 3000),
+(3, '01-FEB-13', 4000),
+(1, '01-JAN-13', 4500),
+(2, '01-JAN-13', 3500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_mast`
+--
+
+CREATE TABLE `item_mast` (
+  `PRO_ID` int(20) NOT NULL,
+  `PRO_NAME` char(30) DEFAULT NULL,
+  `PRO_PRICE` decimal(8,2) DEFAULT NULL,
+  `PRO_COM` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `item_mast`
+--
+
+INSERT INTO `item_mast` (`PRO_ID`, `PRO_NAME`, `PRO_PRICE`, `PRO_COM`) VALUES
+(101, 'Mother Board', 3200.00, 15),
+(102, 'Key Board', 450.00, 16),
+(103, 'ZIP drive', 250.00, 14),
+(104, 'Speaker', 550.00, 16),
+(105, 'Monitor', 5000.00, 11),
+(106, 'DVD drive', 900.00, 12),
+(107, 'CD drive', 800.00, 12),
+(108, 'Printer', 2600.00, 13),
+(109, 'Refill cartridge', 350.00, 13),
+(110, 'Mouse', 250.00, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ord_no` int(20) DEFAULT NULL,
+  `purch_amt` decimal(8,2) DEFAULT NULL,
+  `ord_date` date DEFAULT NULL,
+  `customer_id` int(20) DEFAULT NULL,
+  `salesman_id` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`ord_no`, `purch_amt`, `ord_date`, `customer_id`, `salesman_id`) VALUES
+(70001, 150.50, '2012-10-05', 3005, 5002),
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.50, '2012-08-17', 3009, 5003),
+(70007, 948.50, '2012-09-10', 3005, 5002),
+(70005, 2400.60, '2012-07-27', 3007, 5001),
+(70008, 5760.00, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.40, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.60, '2012-04-25', 3002, 5001);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesman`
+--
+
+CREATE TABLE `salesman` (
+  `Salesman_id` int(11) NOT NULL,
+  `name` char(30) DEFAULT NULL,
+  `city` char(50) DEFAULT NULL,
+  `commision` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `salesman`
+--
+
+INSERT INTO `salesman` (`Salesman_id`, `name`, `city`, `commision`) VALUES
+(5001, 'James Hoog', 'New York', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5003, 'Lauson Hen', 'San Jose', 0.12),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5007, 'Paul Adam', 'Rome', 0.13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesperson`
+--
+
+CREATE TABLE `salesperson` (
+  `SNo` int(11) NOT NULL,
+  `SNAME` char(30) DEFAULT NULL,
+  `CITY` char(50) DEFAULT NULL,
+  `COMM` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `salesperson`
+--
+
+INSERT INTO `salesperson` (`SNo`, `SNAME`, `CITY`, `COMM`) VALUES
+(1001, 'Peel', 'London', 0.12),
+(1002, 'Serres', 'San Jose', 0.13),
+(1003, 'Axelrod', 'New York', 0.10),
+(1004, 'Motika', 'London', 0.11),
+(1007, 'Rafkin', 'Barcelona', 0.15);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `Rollno` int(11) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Branch` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`Rollno`, `Name`, `Branch`) VALUES
+(1, 'Jay', 'Computer Science'),
+(2, 'Suhani', 'Electronic and Com'),
+(3, 'Kriti', 'Electronic and Com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewtable`
+--
+
+CREATE TABLE `viewtable` (
+  `Employee_id` int(11) NOT NULL,
+  `First_name` char(30) DEFAULT NULL,
+  `Last_name` char(50) DEFAULT NULL,
+  `Salary` int(11) DEFAULT NULL,
+  `Joining_date` varchar(30) DEFAULT NULL,
+  `Department` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `viewtable`
+--
+DELIMITER $$
+CREATE TRIGGER `insert_data` AFTER INSERT ON `viewtable` FOR EACH ROW BEGIN
+    -- Insert the new record into ViewTable
+    INSERT INTO employee (Employee_id, First_name, Last_name, Salary, Joining_date, Department)
+    VALUES (NEW.Employee_id, NEW.First_name, NEW.Last_name, NEW.Salary, NEW.Joining_date, NEW.Department);
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CNM`),
+  ADD KEY `SNo` (`SNo`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `city_id` (`city_id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`Employee_id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD KEY `Rollno` (`Rollno`);
+
+--
+-- Indexes for table `incentive`
+--
+ALTER TABLE `incentive`
+  ADD KEY `Employee_ref_id` (`Employee_ref_id`);
+
+--
+-- Indexes for table `item_mast`
+--
+ALTER TABLE `item_mast`
+  ADD PRIMARY KEY (`PRO_ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD KEY `salesman_id` (`salesman_id`);
+
+--
+-- Indexes for table `salesman`
+--
+ALTER TABLE `salesman`
+  ADD PRIMARY KEY (`Salesman_id`);
+
+--
+-- Indexes for table `salesperson`
+--
+ALTER TABLE `salesperson`
+  ADD PRIMARY KEY (`SNo`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`Rollno`);
+
+--
+-- Indexes for table `viewtable`
+--
+ALTER TABLE `viewtable`
+  ADD PRIMARY KEY (`Employee_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `Employee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `item_mast`
+--
+ALTER TABLE `item_mast`
+  MODIFY `PRO_ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`);
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`SNo`) REFERENCES `salesperson` (`SNo`);
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
+
+--
+-- Constraints for table `exam`
+--
+ALTER TABLE `exam`
+  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`Rollno`) REFERENCES `student` (`RollNo`);
+
+--
+-- Constraints for table `incentive`
+--
+ALTER TABLE `incentive`
+  ADD CONSTRAINT `incentive_ibfk_1` FOREIGN KEY (`Employee_ref_id`) REFERENCES `employee` (`Employee_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`salesman_id`) REFERENCES `salesman` (`Salesman_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
